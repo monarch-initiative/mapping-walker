06d08b31d661459569ed6788c01f98761f7a5253
Patrick Kalita <pkalita@lbl.gov>
2022-04-14 16:16:47 -0700

    Convert test files into unittest.TestCase classes

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+import unittest
 
 from linkml_runtime.loaders import json_loader
 from mapping_walker.utils.oxo_utils import load_oxo_payload
@@ -9,11 +10,13 @@ from mapping_walker import __version__
 from mapping_walker.ext_schemas.oxo import Container
 
 
-def test_schema():
-    infile = str(Path(INPUT_DIR) / 'oxo-example-uberon.json')
-    with open(infile, encoding='utf-8') as stream:
-        obj = json.load(stream)
-    print(obj)
-    container = load_oxo_payload(obj)
-    print(container)
+class TestSchema(unittest.TestCase):
+
+    def test_schema(self):
+        infile = str(Path(INPUT_DIR) / 'oxo-example-uberon.json')
+        with open(infile, encoding='utf-8') as stream:
+            obj = json.load(stream)
+        print(obj)
+        container = load_oxo_payload(obj)
+        print(container)
 
